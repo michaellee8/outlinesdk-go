58c55813d0579152e5adbcca891af290f405f937
Michael Lee <ckmichael8@gmail.com>
2019-02-12 15:42:13 +0800

    Add descriptions

--- outlinesdk.go
+++ outlinesdk.go
@@ -1,3 +1,4 @@
 // Package outlinesdk provides a 1:1 mapping to Outline Server's API
+// It implements Outline Server (shadowbox) APIs in https://rebilly.github.io/ReDoc/?url=https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/shadowbox/server/api.yml
 //
 package outlinesdk
